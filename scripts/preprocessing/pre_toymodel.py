720f45b9d9765a36f786874c5802ff712f21c6c3
Santiago Bello <isbelloq@unal.edu.co>
2022-01-15 20:48:28 -0500

    Cambio de lectura de información de pandas a spark y cambio de almacenamiento de parquet a delta

--- scripts/preprocessing/pre_toymodel.py
+++ scripts/preprocessing/pre_toymodel.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+# +
 from tf_entropy.environment.base import get_data_paths
-from tf_entropy.database.io import load_data_to_dataframe, save_dataframe
+from tf_entropy.database.io import load_data_to_spark, save_spark
 import os
+
+import pyspark
+from delta import *
+
+#Configuración de Spark
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+# -
 
 # # Carga de datos
 
@@ -14,14 +27,17 @@ raw_file_path = os.path.join(datalake.raw_path, 'toymodel/data/DATE=2022-01-12')
 raw_file_name = 'austen_books.csv'
 
 bronze_file_path = os.path.join(datalake.bronze_path, 'toymodel/data/YEAR=2022/MONTH=01/DAY=14_22-03')
-bornze_file_name = 'austen.parquet'
+bronze_file_name = 'austen.delta'
 
 #Carga de datos raw
-df = load_data_to_dataframe(
+df = load_data_to_spark(
         lake_path=raw_file_path,
         file_name=raw_file_name,
         sep = ',',
-        encoding='windows-1252')
+        encoding='windows-1252',
+        header='true'
+)
+df = df.toPandas()
 # -
 
 # # Extracción de capítulos (solo válido para toymodel)
@@ -41,7 +57,7 @@ df['chapter'] = df.groupby(['book'])['chapter'].ffill()
 df['chapter'] = (df['volume']+'-').fillna('') + df['chapter'] 
 
 #Borrado de columna auxiliar de volumne
-df.drop(['Unnamed: 0','volume'], axis=1, inplace=True)
+df.drop(['_c0','volume'], axis=1, inplace=True)
 
 #Borrado de valores nulos y reset del index
 df.dropna(inplace=True)
@@ -55,5 +71,6 @@ df.reset_index(drop = True, inplace = True)
 if bronze_file_path not in [*map(lambda x: x[0], os.walk(datalake.bronze_path))]:
     os.makedirs(bronze_file_path)
 
-#Almacenamiento en parquet
-save_dataframe(dataframe=df, lake_path=bronze_file_path, file_name=bornze_file_name)
+#Almacenamiento en delta
+df = spark.createDataFrame(df)
+save_spark(dataframe=df, lake_path=bronze_file_path, file_name=bronze_file_name)
--- tf_entropy/database/io.py
+++ tf_entropy/database/io.py
@@ -2,6 +2,11 @@ from pandas import DataFrame
 import pandas as pd
 import os
 
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql import SparkSession
+
+
+
 def load_data_to_dataframe(lake_path:str, file_name: str, **kwArgs) -> DataFrame:
     """
     Funcion de carga de informacion de datos a dataframe
@@ -39,7 +44,7 @@ def load_data_to_dataframe(lake_path:str, file_name: str, **kwArgs) -> DataFrame
     
     return(df)
 
-def save_dataframe(dataframe:DataFrame, lake_path:str, file_name: str, format: str = 'parquet'):
+def save_dataframe(dataframe:DataFrame, lake_path:str, file_name: str):
     """
     Funcion de almacenamiento de infromación
 
@@ -50,9 +55,7 @@ def save_dataframe(dataframe:DataFrame, lake_path:str, file_name: str, format: s
     lake_path:str
         Ruta de almacenamiento del arachivo
     file_name: str
-        Nombre del archivo de lectura
-    format: str
-        Formato de almacenamiento, por defecto 'parquet'
+        Nombre del archivo de lectura, es necesario que tenga la extención de almacenamiento ejemplo `datos.csv` o `datos.parquet`
     """
     # TODO:
     # FIXME:
@@ -60,6 +63,82 @@ def save_dataframe(dataframe:DataFrame, lake_path:str, file_name: str, format: s
 
     #Definicion de ruta y extension del archivo
     file_path = os.path.join(lake_path, file_name)
+    file_extention = os.path.splitext(file_name)[1][1:]
 
     #Carga de informacion
-    exec(f"df.to_{format}('{file_path}', index = False)", {'df':dataframe})
+    exec(f"df.to_{file_extention}('{file_path}', index = False)", {'df':dataframe})
+
+
+def load_data_to_spark(lake_path:str, file_name: str, **kwArgs) -> SparkDataFrame:
+    """
+    Funcion de carga de informacion de datos a un spark dataframe
+
+    Parameters
+    ----------
+    lake_path:str
+        Ruta de almacenamiento del arachivo
+    file_name: str
+        Nombre del archivo de lectura
+    **kwArgs
+        Valoser de 'option' permitidos por pyspark
+
+    Returns
+    -------
+    df : SparkDataFrame
+        DataFrame con la informacion cargada en spark
+    """
+    # TODO:
+    # FIXME:
+    # TODOC:
+
+    #Definicion de ruta y extension del archivo
+    file_path = os.path.join(lake_path, file_name)
+    file_extention = os.path.splitext(file_name)[1][1:]
+    
+    spark = SparkSession.builder.getOrCreate()
+    
+    #Constucción de expresión a evaluar
+    eval_str = f"spark.read.format('{file_extention}')"
+    for key, value in kwArgs.items():
+        eval_str += f".option('{key}', '{value}')"
+    eval_str += f".load('{file_path}')"
+
+    #Carga de informacion
+    df = eval(eval_str, {'spark':spark})
+    
+    return(df)
+
+def save_spark(dataframe:SparkDataFrame, lake_path:str, file_name: str, mode:str = 'overwrite', **kwArgs):
+    """
+    Funcion de almacenamiento de infromación usando spark
+
+    Parameters
+    ----------
+    dataframe:SparkDataFrame
+        Conjunto de datos a almacenar en spark
+    lake_path:str
+        Ruta de almacenamiento del arachivo
+    file_name: str
+        Nombre del archivo de lectura, es necesario que tenga la extención de almacenamiento ejemplo `datos.csv` o `datos.parquet`
+    mode : str
+        Modo de almacenamiento de información, por defecto 'overwrite'
+    **kwArgs
+        Valoser de 'option' permitidos por pyspark
+    """
+    # TODO:
+    # FIXME:
+    # TODOC:
+
+    #Definicion de ruta y extension del archivo
+    file_path = os.path.join(lake_path, file_name)
+    file_extention = os.path.splitext(file_name)[1][1:]
+
+    #Constucción de expresión a evaluar
+    exec_str = f"df.write"
+    for key, value in kwArgs.items():
+        exec_str += f".option('{key}', '{value}')"
+    exec_str += f".format('{file_extention}').mode('{mode}').save('{file_path}')"
+
+
+    #Ejecución de scrip de almacenamiento
+    exec(exec_str, {'df':dataframe})
